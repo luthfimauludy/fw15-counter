2d433c3f4e7e9e0409ce41094bca2f7e5adc56ee
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-03 15:10:13 +0700

    update: button plus

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 // import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="flex flex-col h-screen justify-center items-center">
       <div>
-        <h1>Count is: </h1>
+        <h1>Count is: {count}</h1>
       </div>
       <div className="flex w-16 justify-between">
         <button className="w-5 h-5 rounded border border-gray-500 flex flex-row justify-center items-center">
           -
         </button>
-        <button className="w-5 h-5 rounded border border-gray-500 flex flex-row justify-center items-center">
+        <button
+          className="w-5 h-5 rounded border border-gray-500 flex flex-row justify-center items-center"
+          onClick={() => setCount((count) => count + 1)}
+        >
           +
         </button>
       </div>
