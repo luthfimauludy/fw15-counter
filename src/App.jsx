f3a0d87a0e7885702acbe4d54b5cf62c2ca16d23
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-03 16:59:47 +0700

    feat: error message and limit condition

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,44 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [error, setError] = useState("");
+
+  const handleCountSum = () => {
+    if (count === 10) {
+      setError("Max 10");
+    } else {
+      setError("");
+      setCount(count + 1);
+    }
+  };
+
+  const handleNumberMin = () => {
+    if (count === 0) {
+      setError("Failed");
+    } else {
+      setError("");
+      setCount(count - 1);
+    }
+  };
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
+    <div className="flex flex-col h-screen justify-center items-center bg-gray-600 gap-5">
       <div>
         <h1>Count is: {count}</h1>
       </div>
-      <div className="flex w-16 justify-between">
+      <div>
+        <h1>{error}</h1>
+      </div>
+      <div className="flex w-32 justify-between">
         <button
-          className="w-5 h-5 rounded border border-gray-500 flex flex-row justify-center items-center"
-          onClick={() => setCount((count) => count - 1)}
+          className="w-10 h-10 rounded border border-black flex flex-row justify-center items-center"
+          onClick={handleNumberMin}
         >
           -
         </button>
         <button
-          className="w-5 h-5 rounded border border-gray-500 flex flex-row justify-center items-center"
-          onClick={() => setCount((count) => count + 1)}
+          className="w-10 h-10 rounded border border-black flex flex-row justify-center items-center"
+          onClick={handleCountSum}
         >
           +
         </button>
